556fe6196b6716abd1803155510ec6fc8ed95d0c
CarlaPerezGavilan <A01023033@itesm.mx>
2019-02-15 13:15:38 -0600

    tercera

---
+++ Evaluaciones/E1_1_Cliente copy/Ceja.java
@@ -0,0 +1,11 @@
+public class Ceja{
+    private String typeEyebrow;
+    private String amountHair;
+
+    public Ceja(){}
+
+    public Ceja(String typeEyebrow, String amountHair){
+        this.typeEyebrow=typeEyebrow;
+        this.amountHair=amountHair;
+    }
+}
